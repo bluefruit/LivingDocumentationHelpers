1f617555dc7d1440f05876d5b9450fb435809160
Byran Wills-Heath <byran@bluefruit.co.uk>
2022-09-15 00:20:16 +0100

    Allow the userneeds page to be written to a specified path instead of having to be in the same directory as the userneeds text file

--- living_documentation_helpers/create_userneeds_page.py
+++ living_documentation_helpers/create_userneeds_page.py
@@ -7,8 +7,8 @@ def main(args=None):
     if args is None:
         args = sys.argv
 
-    if len(args) != 2:
-        print('Invalid command format, format is:\n {a[0]} <userneed text file>\n'.format(a=args))
+    if len(args) != 3:
+        print('Invalid command format, format is:\n {a[0]} <userneed text file> <userneeds sphinx file>\n'.format(a=args))
         exit(1)
 
     userneeds = load_userneeds_from_file(args[1])
@@ -17,10 +17,7 @@ def main(args=None):
         "userneeds": userneeds
     }
 
-    (file_name, _) = path.splitext(args[1])
-    file_name += '.rst'
-
-    with open(file_name, 'wt') as file:
+    with open(args[2], 'wt') as file:
         file.write(convert_template('userneeds.jinja2', context))
 
 
